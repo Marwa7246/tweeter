b1cb48f3a9a08353f386f0639cc2d3d087bcdd71
Marwa Ragheb <marwa.ragheb@gmail.com>
2020-08-12 16:54:55 +0000

    Use find method in the characCount function instead of counter class

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,20 +2,22 @@
 const $input = $('#tweet-text');
 let $counter = $('.counter').val();
 
+// COUNT THE NUMBER OF REMAINING CHARACTERS AND MAKE THE COUNTER RED IF IT REACHES 0
 
+function characCount () {
 
-let count = 0;
+  const tweetLength = $(this).val().length;
+  const count = $(this).parent().find('.counter');
+  count.removeClass('red');
+
+  count.val(140 - tweetLength);
+
+  if (tweetLength >= 140) {
+    count.addClass('red');
+  }
+
+}
+
 $(document).ready(() => {
-  $input.on('keyup', function() {
-
-    const tweetLength = $(this).val().length;
-    console.log('key: ' , $(this).val());
-    $('.counter').removeClass('red');
-  
-    $('.counter').val(140 - tweetLength);
-    if (tweetLength >= 140) {
-      $('.counter').addClass('red');
-    }
-
-  });
+  $input.on('keyup', characCount);
 });
