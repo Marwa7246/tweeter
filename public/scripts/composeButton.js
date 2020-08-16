6a7948fdc3882516d6eba48460b2fd513076513a
Marwa Ragheb <marwa.ragheb@gmail.com>
2020-08-16 06:41:54 +0000

    Add asecond button at the end of the page to return to the top on click

--- public/scripts/composeButton.js
+++ public/scripts/composeButton.js
@@ -1,13 +1,35 @@
 //If the user click on the compse button in the navigation---> go to form and focus in the textarea
 
 $(document).ready(() => {
-  $('#compose').on('click', composeNew);  
+  $('#compose').on('click', composeNew);
 });
 function composeNew() {
-   if ($('.new-tweet').css('display') === 'none'){ 
-     $('.new-tweet').slideDown() 
-     $('#tweet-text').focus()
-    } else {
-      $('.new-tweet').slideUp() 
-    }
+  if ($('.new-tweet').css('display') === 'none') {
+    $('.new-tweet').slideDown();
+    $('#tweet-text').focus();
+  } else {
+    $('.new-tweet').slideUp();
   }
+}
+
+
+$(document).ready(() => {
+   window.onscroll = function() {
+    scrollFunction();
+  };
+  $('#scroll').on('click', topFunction);
+});
+
+// When the user scrolls down 100px from the top of the document, show the button
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    $('#scroll').show(400);
+  } else {
+    $('#scroll').hide(400);
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
